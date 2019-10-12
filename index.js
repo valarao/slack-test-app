ff13eaa0ea33915e9d09a412af7c03373ae1d20d
Miguel Valarao <miguel.valarao@gmail.com>
2019-10-11 20:34:52 -0700

    Add joke bot

--- index.js
+++ index.js
@@ -4,16 +4,51 @@ require('dotenv').config();
 
 const bot = new SlackBot({
     token: process.env.SLACK_BOT_TOKEN,
-    name: 'jokebot',
+    name: 'test_app',
 })
 
-// Start Handler
 bot.on('start', () => {
     const params = {
         icon_emoji: ':smiley:'
     };
 
-    bot.postMessageToChannel('general', 'Get Ready To Laugh With @Jokebot!', params);
+    bot.postMessageToChannel(
+        'general',
+        'Get Ready To Laugh With @Jokebot!', 
+        params
+    );
 });
 
-console.log(process.env.SLACK_BOT_TOKEN);
+bot.on('error', (err) => console.log(err));
+
+bot.on('message', data => {
+    if (data.type !== 'message') {
+        return;
+    }
+
+    handleMessage(data.text);
+});
+
+function handleMessage(message) {
+    console.log(message);
+    if (message.includes(' chucknorris')) {
+        chuckJoke();
+    }
+}
+
+function chuckJoke() {
+    axios.get('https://api.icndb.com/jokes/random')
+        .then(res => {
+            const joke = res.data.value.joke;
+
+            const params = {
+                icon_emoji: ':laughing:'
+            };
+        
+            bot.postMessageToChannel(
+                'general',
+                `Chuck Norris: ${joke}`, 
+                params
+            );
+        })
+}
